3e5a1a754d283050d102a845c1e7e25f7bdea27a
sonasingh46 <sonasingh46@gmail.com>
2018-02-25 19:03:17 +0530

    Adding Comments and Inlining Varaible Declarations

--- search/linear-search.go
+++ search/linear-search.go
@@ -1,36 +1,38 @@
+//Linear Search
+//Note: If there is duplicate entry, the search will return the very first element
 package main
 
 import (
 	"fmt"
 )
 
+//Linear Search Function
 func linearSearch(searchArray []int, searchElement int) int {
-	for i, item := range searchArray{
-	    if item == searchElement{
-		    return i
+	for i, item := range searchArray {
+		if item == searchElement {
+			return i
 		}
 	}
-    return -1
+	return -1
 }
 
-func main(){
-    var item int
-    var size int
-    fmt.Println("Enter size of array.")
-    fmt.Scan(&size)
-    var items = make([]int,size)
-    fmt.Println("Enter elements of array")
-    for i :=0; i<size; i++ {
-        fmt.Scan(&item)
-        items[i] = item
-    }
-    var key int
-    fmt.Println("Enter key which you want to search")
-    fmt.Scan(&key)
-    index := linearSearch(items,key)
-    if index == -1 {
-        fmt.Println("item not found!")
-    }else{
-        fmt.Printf("%d found at position %d ",items[index],index+1)
-    }
+func main() {
+	var item, size int
+	fmt.Println("Enter size of array:")
+	fmt.Scan(&size)
+	var array = make([]int, size)
+	fmt.Println("Enter elements of array:")
+	for i := 0; i < size; i++ {
+		fmt.Scan(&item)
+		array[i] = item
+	}
+	var key int
+	fmt.Println("Enter key which you want to search:")
+	fmt.Scan(&key)
+	index := linearSearch(array, key)
+	if index == -1 {
+		fmt.Println("Item not found!")
+	} else {
+		fmt.Printf("%d found at position %d\n", array[index], index+1)
+	}
 }
